fea38f332e8a992500383dd0d552e24797786cd0
Chhavi Rohilla <136676662+ChhaviRohilla@users.noreply.github.com>
2024-08-14 14:52:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,5 +33,5 @@ app.post('/summarize', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log('Server running at http://localhost:${port}/');
+  console.log('Server running at http://localhost:3000/');
 });
